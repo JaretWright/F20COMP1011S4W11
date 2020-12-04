261824b82e143a5def89e51a5cfa8ebb97692c8a
Jaret Wright <hkkA223>
2020-12-04 12:15:46 -0500

    Added enough code to read the Json file

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import Models.ApiResponseModel;
 import Models.MovieInfo;
+import Utilities.JsonFileUtility;
+
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-//        MovieInfo myMovie = new MovieInfo();
+        ApiResponseModel apiResponse = JsonFileUtility.getApiInfoFromJson(new File("src/Utilities/movies.json"));
+        System.out.println();
     }
 }
