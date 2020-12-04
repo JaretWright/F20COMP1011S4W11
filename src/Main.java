89a4f378b8b0967cc02fc7f5e12dcdc7e534bbfb
Jaret Wright <hkkA223>
2020-12-04 11:44:06 -0500

    Added JSON file

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,7 @@
+import Models.MovieInfo;
+
 public class Main {
+    public static void main(String[] args) {
+//        MovieInfo myMovie = new MovieInfo();
+    }
 }
--- src/Models/MovieInfo.java
+++ src/Models/MovieInfo.java
@@ -3,8 +3,8 @@ package Models;
 import com.google.gson.annotations.SerializedName;
 
 public class MovieInfo {
-    @SerializedName("Title")
-    private String title;
+    @SerializedName("Title")  //This is the name in the JSON file
+    private String title;     //This is the variable name in Java
 
     @SerializedName("Year")
     private String year;
@@ -17,4 +17,61 @@ public class MovieInfo {
     @SerializedName("Poster")
     private String poster;
 
+    public MovieInfo(String title, String year, String imdbID, String type, String poster) {
+        setTitle(title);
+        setYear(year);
+        setImdbID(imdbID);
+        setType(type);
+        setPoster(poster);
+    }
+
+    public void validateInstanceVariables()
+    {
+        setTitle(title);
+        setYear(year);
+        setImdbID(imdbID);
+        setType(type);
+        setPoster(poster);
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getImdbID() {
+        return imdbID;
+    }
+
+    public void setImdbID(String imdbID) {
+        this.imdbID = imdbID;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
 }
