029406f48e36279d4fbe26a6db4d9189f77455c5
Jaret Wright <hkkA223>
2020-12-04 12:40:29 -0500

    updated with the latest movie info

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 import Models.ApiResponseModel;
 import Models.MovieInfo;
 import Utilities.JsonFileUtility;
+import Utilities.OMDBApiUtility;
 
 import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-        ApiResponseModel apiResponse = JsonFileUtility.getApiInfoFromJson(new File("src/Utilities/movies.json"));
+//        ApiResponseModel apiResponse = JsonFileUtility.getApiInfoFromJson(new File("src/Utilities/movies.json"));
+        OMDBApiUtility.getMovieFromSearch("Howard");
         System.out.println();
     }
 }
--- src/Utilities/OMDBApiUtility.java
+++ src/Utilities/OMDBApiUtility.java
@@ -1,5 +1,6 @@
 package Utilities;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -12,6 +13,11 @@ public class OMDBApiUtility {
 
     public static void getMovieFromSearch(String searchText)
     {
+        //precautionary step to clear out the old file.
+        File file = new File("src/Utilities/movies.json");
+        if (file.exists())
+            file.delete();
+
         String searchURL = "http://www.omdbapi.com/?apikey=4a1010ab&s="+searchText;
 
         HttpClient client = HttpClient.newHttpClient();
@@ -20,6 +26,7 @@ public class OMDBApiUtility {
         try {
             HttpResponse<Path> response = client.send(request, HttpResponse.BodyHandlers.ofFile(
                                                             Paths.get("src/Utilities/movies.json")));
+            System.out.println();
         } catch (IOException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
